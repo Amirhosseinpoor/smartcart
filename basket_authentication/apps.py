31b5391aea9ad5e0485e5249f47d55f6fda38f90
Amirhosseinpoor <amirhosseinpoor@example.com>
2024-12-07 19:12:29 +0330

    created new apps

---
+++ basket_authentication/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BasketAuthenticationConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'basket_authentication'
---
+++ basket_display_blog/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BasketDisplayBlogConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'basket_display_blog'
