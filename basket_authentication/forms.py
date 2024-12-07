541c218ad60fc294cf2caa96dda7deefebb180f4
Amirhosseinpoor <amirhosseinpoor@example.com>
2024-12-07 20:17:25 +0330

    add v1 models

---
+++ basket_authentication/forms.py
@@ -0,0 +1,2 @@
+class CustomUserCreationForm():
+    pass
--- config/urls.py
+++ config/urls.py
@@ -16,8 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('basket_display_blog.urls')),
+    path('', include('django.contrib.auth.urls')),
+    path('', include('basket_authentication.urls')),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
